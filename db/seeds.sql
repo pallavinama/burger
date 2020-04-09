8e5e93d9459cf5080bd2568b895bd0c87fb5aa26
pallavi <pallavi_nama@yahoo.com>
2020-04-09 02:13:30 -0400

    Updated sql files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger) VALUES ('cheeseBurger','true');
+INSERT INTO burgers (burger) VALUES ('chili burger','true');
+INSERT INTO burgers (burger) VALUES ('Salmon-burger','true');
